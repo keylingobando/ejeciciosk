45df6581980f8896f4bfe5da1de635ce4466b51e
keyling <keyling.obandofonseca@tiernoparla.es>
2022-10-26 18:10:17 +0200

    numeros pares del 1 al 10

---
+++ practica1/src/main/java/keypractica.java
@@ -0,0 +1,17 @@
+
+
+public class keypractica {
+
+    public static void main(String[] args) {
+        //printear numeros pares del 1 al 10
+        int num = 10;
+        int tem = 0;
+        for (int i = 0; i <= 10; i++) {
+            if (i % 2 == 0) {
+                System.out.println("valor par = " + i);
+            }
+        }
+
+    }
+}
+
